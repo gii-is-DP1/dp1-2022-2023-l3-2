e72fef97cfb0aaf74da9ec2721740e79693f947d
alegarsan11 <alegarsan11@alum.us.es>
2022-11-06 18:03:04 +0100

    Entidad de pieza

---
+++ src/main/java/org/springframework/samples/dwarf/tablero/Pieza.java
@@ -0,0 +1,22 @@
+package org.springframework.samples.dwarf.tablero;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Pieza {
+
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Integer Posicion;
+    
+}
