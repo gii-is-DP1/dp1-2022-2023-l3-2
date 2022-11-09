8e17e41605592cfe30ae153813d2203122f598d4
alegarsan11 <alegarsan11@alum.us.es>
2022-11-09 19:44:41 +0100

    Actualizaciones Jugador y tablero

--- src/main/java/org/springframework/samples/dwarf/tablero/Enano.java
+++ src/main/java/org/springframework/samples/dwarf/tablero/Enano.java
@@ -4,6 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.springframework.samples.dwarf.jugador.Jugador;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -19,4 +23,11 @@ public class Enano {
 
     private Integer Posicion;
     
+    @ManyToOne
+    @JoinColumn(name = "jugador_id")
+    private Jugador jugador;
+
+    @ManyToOne
+    @JoinColumn(name="carta_id")
+    private Carta carta;
 }
