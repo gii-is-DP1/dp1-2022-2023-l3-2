18bed18d162bc48891ac8315ef15f3ce30598215
rafgargal <rafgargal@alum.us.es>
2023-01-11 19:43:59 +0100

    JugadorService test y correcion UserService test

--- src/test/java/org/springframework/samples/dwarf/jugador/JugadorServiceTest.java
+++ src/test/java/org/springframework/samples/dwarf/jugador/JugadorServiceTest.java
@@ -1,14 +1,15 @@
 package org.springframework.samples.dwarf.jugador;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
-import org.junit.jupiter.api.BeforeEach;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.samples.dwarf.user.User;
+import org.springframework.samples.dwarf.user.UserService;
 import org.springframework.stereotype.Service;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
@@ -17,25 +18,87 @@ public class JugadorServiceTest {
     @Autowired
     protected JugadorService jugadorService;
 
-    @BeforeEach
-    public void config(){
-        Jugador jug = new Jugador();
-        User user = new User();
+    @Autowired
+    protected UserService userService;
 
-        user.setUsername("TestUser");
-        user.setEnabled(true);
+    @Test
+    public void shouldFindOwnerById() {
+        Jugador alegarsan11 = jugadorService.findOwnerById(1);
+        User alegarsan11User = userService.findUser("alegarsan11").get();
 
-        jug.setId(999);
-        jug.setUser(user);
+        assertThat(alegarsan11.getAcero()).isEqualTo(0);
+        assertThat(alegarsan11.getHierro()).isEqualTo(0);
+        assertThat(alegarsan11.getMedalla()).isEqualTo(0);
+        assertThat(alegarsan11.getObjeto()).isEqualTo(0);
+        assertThat(alegarsan11.getOro()).isEqualTo(0);
+        assertThat(alegarsan11.getPosicionFinal()).isEqualTo(null);
+        assertThat(alegarsan11.getEnanosDisponibles()).isEqualTo(2);
+
+        assertThat(alegarsan11.isEsespectador()).isEqualTo(false);
+        assertThat(alegarsan11.isPrimerjugador()).isEqualTo(true);
+        assertThat(alegarsan11.isTurno()).isEqualTo(true);
+
+        assertThat(alegarsan11.getUser()).isEqualTo(alegarsan11User);
     }
 
-    /* @Test
-    public void shouldFindOwnerById() {
-        Jugador jugador = this.jugadorService.findOwnerById(999);
-		assertThat(jugador.getUser().getUsername().equals("TestUser"));
-		Jugador jugador1 = this.jugadorService.findOwnerById(154);
-		assertThat(jugador1).isNull();
-    } */
+    @Test
+    public void shouldFindAll() {
+        List<Jugador> jugadores = jugadorService.findAll();
+
+        Jugador alegarsan11 = jugadores.get(0);
+        assertThat(alegarsan11.getAcero()).isEqualTo(0);
+        assertThat(alegarsan11.getHierro()).isEqualTo(0);
+        assertThat(alegarsan11.getMedalla()).isEqualTo(0);
+        assertThat(alegarsan11.getObjeto()).isEqualTo(0);
+        assertThat(alegarsan11.getOro()).isEqualTo(0);
+        assertThat(alegarsan11.getPosicionFinal()).isEqualTo(null);
+        assertThat(alegarsan11.getEnanosDisponibles()).isEqualTo(2);
+
+        assertThat(alegarsan11.isEsespectador()).isEqualTo(false);
+        assertThat(alegarsan11.isPrimerjugador()).isEqualTo(true);
+        assertThat(alegarsan11.isTurno()).isEqualTo(true);
+
+        User alegarsan11User = userService.findUser("alegarsan11").get();
+        assertThat(alegarsan11.getUser()).isEqualTo(alegarsan11User);
+    }
+
+    @Test
+    public void shouldFindJugadorUser() {
+        List<Jugador> jugadores = jugadorService.findJugadorUser("alegarsan11");
+
+        Jugador alegarsan11 = jugadores.get(0);
+        assertThat(alegarsan11.getAcero()).isEqualTo(0);
+        assertThat(alegarsan11.getHierro()).isEqualTo(0);
+        assertThat(alegarsan11.getMedalla()).isEqualTo(0);
+        assertThat(alegarsan11.getObjeto()).isEqualTo(0);
+        assertThat(alegarsan11.getOro()).isEqualTo(0);
+        assertThat(alegarsan11.getPosicionFinal()).isEqualTo(null);
+        assertThat(alegarsan11.getEnanosDisponibles()).isEqualTo(2);
+
+        assertThat(alegarsan11.isEsespectador()).isEqualTo(false);
+        assertThat(alegarsan11.isPrimerjugador()).isEqualTo(true);
+        assertThat(alegarsan11.isTurno()).isEqualTo(true);
+
+        User alegarsan11User = userService.findUser("alegarsan11").get();
+        assertThat(alegarsan11.getUser()).isEqualTo(alegarsan11User);
+    }
+
+    @Test
+    public void shouldCreateJugadorByUsername() {
+        Jugador alegarsan12 = jugadorService.createJugadorByUsername("alegarsan11", false);
+
+        assertThat(alegarsan12.getId()).isEqualTo(2);
+        assertThat(alegarsan12.isPrimerjugador()).isEqualTo(false);
+        User alegarsan11User = userService.findUser("alegarsan11").get();
+        assertThat(alegarsan12.getUser()).isEqualTo(alegarsan11User);
+    }
+
+    @Test
+    public void shouldDeleteJugador() {
+        Jugador alegarsan11 = jugadorService.findOwnerById(1);
+        jugadorService.deleteJugador(alegarsan11);
+        assertThat(jugadorService.findAll().size()).isEqualTo(0);
+    }
 
 }
 
