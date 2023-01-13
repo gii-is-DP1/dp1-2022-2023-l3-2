023bea13cfd3598edbf6c75aa265f545d4841dd5
jualeomad <jualeomad@alum.us.es>
2023-01-13 13:38:42 +0100

    Correccion jugadorServiceTest

--- src/test/java/org/springframework/samples/dwarf/jugador/JugadorServiceTest.java
+++ src/test/java/org/springframework/samples/dwarf/jugador/JugadorServiceTest.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
 public class JugadorServiceTest {
-    
+
     @Autowired
     protected JugadorService jugadorService;
 
@@ -87,7 +87,7 @@ public class JugadorServiceTest {
     public void shouldCreateJugadorByUsername() {
         Jugador alegarsan12 = jugadorService.createJugadorByUsername("alegarsan11", false);
 
-        assertThat(alegarsan12.getId()).isEqualTo(2);
+        assertThat(alegarsan12.getId()).isEqualTo(22);
         assertThat(alegarsan12.isPrimerjugador()).isEqualTo(false);
         User alegarsan11User = userService.findUser("alegarsan11").get();
         assertThat(alegarsan12.getUser()).isEqualTo(alegarsan11User);
@@ -101,4 +101,3 @@ public class JugadorServiceTest {
     }
 
 }
-
